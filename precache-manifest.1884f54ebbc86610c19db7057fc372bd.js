701e78769ec74958c0fcc2228671a575cae988f9
qhuynh <qhuynh@bocasay.com>
2020-08-19 16:30:52 +0700

    Updates

---
+++ precache-manifest.1884f54ebbc86610c19db7057fc372bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47d398c8f49fadee5127b2bef07ac7c",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "050764a104cc4b7772e0",
+    "url": "/react-quiz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c348fabcb8d9c3d9d07a",
+    "url": "/react-quiz/static/js/2.85798b1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.85798b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050764a104cc4b7772e0",
+    "url": "/react-quiz/static/js/main.19152dfd.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/react-quiz/static/media/background.c11f97a3.jpg"
+  }
+]);
